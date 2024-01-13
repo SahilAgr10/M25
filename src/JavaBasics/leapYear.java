70e4924a624ec0c21f52949412a9819d8b7c4030
Sahil Agrawal <agrawalsahil4994@gmail.com>
2024-01-14 00:08:02 +0530

    leap year

---
+++ src/JavaBasics/leapYear.java
@@ -0,0 +1,19 @@
+package JavaBasics;
+
+import java.util.*;
+
+//Input a year and find whether it is a leap year or not.
+public class leapYear {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int lYear = sc.nextInt();
+        if(isLeapYear(lYear)){
+            System.out.println("It's a Leap Year");
+        } else{
+            System.out.println("not a leap year");
+        }
+    }
+    public static boolean isLeapYear(int year){
+        return (year % 4 == 0) && ((year % 100 != 0) || (year % 400 == 0));
+    }
+}
