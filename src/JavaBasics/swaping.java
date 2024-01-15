b02a9c1b41bacc3a25b9d27fd17c28a0309a38bc
Sahil Agrawal <agrawalsahil4994@gmail.com>
2024-01-15 19:42:58 +0530

    swapping

--- src/JavaBasics/swaping.java
+++ src/JavaBasics/swaping.java
@@ -9,9 +9,12 @@ public class swaping {
         System.out.println(" Enter two values");
         int a = sc.nextInt();
         int b = sc.nextInt();
-
+        System.out.println(" Before :");
         System.out.println(" a = "+ a + " b = "+ b);
-        swap2(a,b);
+        System.out.println(" After :");
+        swap(a,b);
+        swapWithoutVariable(a,b);
+        swapUsingBitOperator(a,b);
     }
     public static void swap(int a , int b){
         int temp = a;
@@ -21,10 +24,17 @@ public class swaping {
         System.out.println(" a = "+ a + " b = "+ b);
     }
     // Swapping without creating variable
-    public static void swap2(int a , int b){
+    public static void swapWithoutVariable(int a , int b){
         a = a-b; // assume a = 5 , b = 3
-        b = a+b; // here a will be 2
-        a = b-a;
+        b = a+b; // here a will be 2 , b = 5
+        a = b-a; // here a will be 3
+        System.out.println(" a = "+ a + " b = "+ b);
+    }
+    // Swapping with bit operator
+    public static void swapUsingBitOperator(int a , int b){
+        a = a^b;
+        b = a^b;
+        a = a^b;
         System.out.println(" a = "+ a + " b = "+ b);
     }
 }
