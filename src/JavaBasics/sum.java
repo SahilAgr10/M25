dcae0f5d56231c8ce0c660d327848bc0d358bdf9
Sahil Agrawal <agrawalsahil4994@gmail.com>
2024-03-04 22:20:33 +0530

    sum of n

--- src/JavaBasics/sum.java
+++ src/JavaBasics/sum.java
@@ -1,4 +1,22 @@
 package JavaBasics;
 
+import java.util.Scanner;
+
 public class sum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+
+        while(true){
+            String input = sc.nextLine();
+            if(input.equalsIgnoreCase("x")) break;
+            try {
+                int number = Integer.parseInt(input);
+                sum += number;
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. Please enter a number or 'x' to finish.");
+            }
+        }
+        System.out.println("Sum of all numbers entered: " + sum);
+    }
 }
