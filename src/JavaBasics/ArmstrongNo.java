23dffe955c735fba0929da2a5fed717ef5fc22a7
Sahil Agrawal <agrawalsahil4994@gmail.com>
2024-03-06 01:54:51 +0530

    armstrong num

--- src/JavaBasics/ArmstrongNo.java
+++ src/JavaBasics/ArmstrongNo.java
@@ -1,15 +1,11 @@
 package JavaBasics;
-
 import java.util.Scanner;
-
 public class ArmstrongNo {
     public static void main(String[] args) {
-       // System.out.println((int)(Math.log10(12)) + 1 );
         Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-        if(isArmstrong(num)) System.out.println(" it's a Armstrong number");
-        else System.out.println(" not a Armstrong num");
-    }
+        for(int i =100;i<=999;i++){
+            if(isArmstrong(i)) System.out.println(i);
+        }}// System.out.println((int)(Math.log10(12)) + 1 );
     static boolean isArmstrong(int num){
         int orignal = num;
         int sum = 0;
@@ -18,7 +14,6 @@ public class ArmstrongNo {
             sum+=rem*rem*rem;
             num/=10;
         }
-        if(sum==orignal) return true;
-        return false;
+        return sum == orignal;
     }
 }
