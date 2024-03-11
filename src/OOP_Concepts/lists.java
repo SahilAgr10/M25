2492d99e481e237814a2f5556247683b9443ae2d
Sahil Agrawal <agrawalsahil4994@gmail.com>
2024-03-12 02:47:46 +0530

    Algo Updates

--- src/OOP_Concepts/lists.java
+++ src/OOP_Concepts/lists.java
@@ -5,6 +5,14 @@ import java.util.Arrays;
 
 public class lists {
     public static void main(String[] args) {
-
+        ArrayList<Places> lists = new ArrayList<>();
+        lists.add(new Places("Mumbai","12/01/2021"));
+        lists.add(new Places("Mumbai","12/01/2021"));
+        lists.add(new Places("Mumbai","12/01/2021"));
+        Places place = new Places();
+        place.setPlace("Bombay");
+        place.setDate("20/11/23");
+        lists.add(place);
+        System.out.println(lists);
     }
 }
