904811ed3e413d6a1b2f6a258b3fa88ff498b25d
Sahil Agrawal <agrawalsahil4994@gmail.com>
2024-03-12 02:46:41 +0530

    Algo Updates

--- src/Algorithms/SearchingAlgo/Binary_Search.java
+++ src/Algorithms/SearchingAlgo/Binary_Search.java
@@ -1,4 +1,27 @@
 package Algorithms.SearchingAlgo;
 
 public class Binary_Search {
+    public static void main(String[] args) {
+        int[] arr = {9,8,7,6,5,4,3,2,1};
+        int target = 7;
+        orderAgnosticSearch(arr,target);
+    }
+
+    private static void orderAgnosticSearch(int[] arr, int target) {
+        int start =0;
+        int end = arr.length-1;
+        boolean isAsc = arr[start]<arr[end];
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(arr[mid]==target) System.out.println(mid);
+            if(isAsc){
+                if(target>arr[mid]) start = mid+1;
+                else end = mid-1;
+            }else{
+                if(target>arr[mid]) end = mid-1;
+                else start = mid+1;
+            }
+        }
+        System.out.println(-1);
+    }
 }
