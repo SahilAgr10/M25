8f9d3aa25997ba784e6e7481245f0c9b217401c0
Sahil Agrawal <agrawalsahil4994@gmail.com>
2024-03-11 14:53:46 +0530

    Algo Updates

--- src/Data_Structures/Arrays/Transpose.java
+++ src/Data_Structures/Arrays/Transpose.java
@@ -1,4 +1,14 @@
 package Data_Structures.Arrays;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Transpose {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("A");
+        list.add("B");
+        list.add("C");
+        System.out.println(list);
+    }
 }
